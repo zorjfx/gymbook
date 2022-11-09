3f040a50e666e3356e0249241814763e76eb1a8e
zorjfx <r.a.zhilenko08@gmail.com>
2022-11-09 22:00:03 +0600

    homework 34(task b only)

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
     <!-- <VueHeader></VueHeader> -->
     <!-- <CryptoPrice ticker="BTCUSDT"></CryptoPrice> -->
-    <crypto-trade @deal-closed="(order) => closedDeal = order"></crypto-trade>
-    <crypto-history :order="closedDeal"></crypto-history>
+    <!-- <crypto-trade @deal-closed="(order) => closedDeal = order"></crypto-trade>
+    <crypto-history :order="closedDeal"></crypto-history> -->
+    <login-form></login-form>
 </template>
 
 <script>
@@ -12,12 +13,13 @@ import CryptoPrice from './components/CryptoPrice.vue';
 import CryptoTrade from './components/CryptoTrade.vue';
 import CryptoSelection from './components/CryptoSelection.vue';
 import CryptoHistory from './components/CryptoHistory.vue';
+import LoginForm from './components/LoginForm.vue';
 import { ref } from 'vue';
 
-const closedDeal = ref(null);
+// const closedDeal = ref(null);
 
 export default {
-    components: { VueHeader, CryptoPrice, CryptoTrade, CryptoSelection, CryptoHistory }
+    components: { VueHeader, CryptoPrice, CryptoTrade, CryptoSelection, CryptoHistory, LoginForm }
 }
 
 
---
+++ src/components/LoginForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Create an account</h2>
+        <input type="text" placeholder="Username" v-model="username">
+        <input type="text" placeholder="Password" v-model="password">
+        <button @click="dataPost()">Submit</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const username = ref(null);
+const password = ref(null);
+
+async function dataPost() {
+    const result = await axios.post('https://test.com/login', { username: username.value, password: password.value });
+}
+
+</script>
+
+<style>
+
+</style>
